c82052ed91e686116fe066763f090d2fa9455c2a
Benedicte Raae <raae@bgraphic.no>
2014-08-02 19:02:57 +0200

    Added support newest font awsome, solves #1

--- wp-content/themes/a2-radius-child/functions.php
+++ wp-content/themes/a2-radius-child/functions.php
@@ -1,3 +1,14 @@
 <?php
 
 require( get_stylesheet_directory() . '/includes/widgets/text-column.php' );
+
+/* Enqueue Scripts and Styles */
+function a2_scripts_styles() {
+
+  $version = wp_get_theme()->Version;
+
+  //Font Awesome CSS
+  wp_enqueue_style( 'font-awesome-css-4', get_stylesheet_directory_uri() . "/includes/fonts/font-awesome-4.1.0/css/font-awesome.min.css", array(), $version, 'screen' );
+
+}
+add_action( 'wp_enqueue_scripts', 'a2_scripts_styles' );
