9215795cb6db00a87685e8b2982fafd9bc0b972d
Benedicte Raae <raae@bgraphic.no>
2014-08-24 20:12:57 +0200

    Show all portfolio items

--- wp-content/themes/a2-radius-child/lib/loop-modifications.php
+++ wp-content/themes/a2-radius-child/lib/loop-modifications.php
@@ -4,14 +4,22 @@
 function portfolio_loop_modifications( $query ) {
   if ( $query->is_post_type_archive('array-portfolio')) {
     $query->set( 'orderby', 'modified' );
+
+    if($query->is_main_query()) {
+      $query->set( 'posts_per_page', -1 );
+    }
   }
+
 }
 add_action( 'pre_get_posts', 'portfolio_loop_modifications' );
 
 /* Show all employees */
 function employee_loop_modifications( $query ) {
   if ( $query->is_post_type_archive('a2-employee')) {
-    $query->set( 'posts_per_page', -1 );
+    if($query->is_main_query()) {
+      $query->set( 'posts_per_page', -1 );
+    }
+
   }
 }
 add_action( 'pre_get_posts', 'employee_loop_modifications' );
