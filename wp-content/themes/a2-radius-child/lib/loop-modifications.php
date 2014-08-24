bea3389809fc7029783391e7a833dbdfa932543f
Benedicte Raae <raae@bgraphic.no>
2014-08-24 21:41:08 +0200

    Order by portfolio items by published date again

--- wp-content/themes/a2-radius-child/lib/loop-modifications.php
+++ wp-content/themes/a2-radius-child/lib/loop-modifications.php
@@ -3,8 +3,6 @@
 /* Sort portfolio on last modified */
 function portfolio_loop_modifications( $query ) {
   if ( $query->is_post_type_archive('array-portfolio')) {
-    $query->set( 'orderby', 'modified' );
-
     if($query->is_main_query()) {
       $query->set( 'posts_per_page', -1 );
     }
