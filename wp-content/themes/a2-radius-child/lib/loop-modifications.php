029ca996dcec1b9ece165469ed03380d45a7ae83
Benedicte Raae <raae@bgraphic.no>
2014-08-24 19:13:03 +0200

    Show all employees #5

--- wp-content/themes/a2-radius-child/lib/loop-modifications.php
+++ wp-content/themes/a2-radius-child/lib/loop-modifications.php
@@ -1,9 +1,17 @@
 <?php
 
 /* Sort portfolio on last modified */
-function last_modified_portfolio( $query ) {
-    if ( $query->is_post_type_archive('array-portfolio')) {
-        $query->set( 'orderby', 'modified' );
-    }
+function portfolio_loop_modifications( $query ) {
+  if ( $query->is_post_type_archive('array-portfolio')) {
+    $query->set( 'orderby', 'modified' );
+  }
 }
-add_action( 'pre_get_posts', 'last_modified_portfolio' );
+add_action( 'pre_get_posts', 'portfolio_loop_modifications' );
+
+/* Show all employees */
+function employee_loop_modifications( $query ) {
+  if ( $query->is_post_type_archive('a2-employee')) {
+    $query->set( 'posts_per_page', -1 );
+  }
+}
+add_action( 'pre_get_posts', 'employee_loop_modifications' );
