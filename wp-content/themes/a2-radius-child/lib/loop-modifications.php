cb0c57e2edcad8e68343c006fabf5a2aa828045b
Benedicte Raae <raae@bgraphic.no>
2014-09-08 16:46:07 +0200

    Sort employees on title (name) #5

--- wp-content/themes/a2-radius-child/lib/loop-modifications.php
+++ wp-content/themes/a2-radius-child/lib/loop-modifications.php
@@ -14,10 +14,13 @@ add_action( 'pre_get_posts', 'portfolio_loop_modifications' );
 /* Show all employees */
 function employee_loop_modifications( $query ) {
   if ( $query->is_post_type_archive('a2-employee')) {
+
+    $query->set( 'orderby', 'title');
+    $query->set( 'order', 'ASC');
+
     if($query->is_main_query()) {
       $query->set( 'posts_per_page', -1 );
     }
-
   }
 }
 add_action( 'pre_get_posts', 'employee_loop_modifications' );
